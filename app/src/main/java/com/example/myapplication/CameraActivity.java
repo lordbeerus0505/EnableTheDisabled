fdf2f62c5d3bbffc9075b5e388f1ad4cd4a0cb14
ckarthik14 <ckarthik114@gmail.com>
2019-10-25 23:58:45 +0530

    Added intent to start IP camera

--- app/src/main/java/com/example/myapplication/CameraActivity.java
+++ app/src/main/java/com/example/myapplication/CameraActivity.java
@@ -2,18 +2,62 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class CameraActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
-        startActivity(new Intent(CameraActivity.this, CameraShoot.class));
+
+        final OkHttpClient client = new OkHttpClient();
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("chatParticipants", UserDetails.username+'_'+UserDetails.chatWith)
+                .build();
+
+        final Request request = new Request.Builder()
+                .url("http://192.168.43.13:5000")
+                .post(formBody)
+                .build();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Response response = client.newCall(request).execute();
+
+                    if(response.code() == 200)
+                        response.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.pas.webcam");
+        if (launchIntent != null) {
+            startActivity(launchIntent);//null pointer check in case package name was not found
+        }
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            startActivityForResult(takePictureIntent, 1);
+//        }
 //        Button btn = (Button)findViewById(R.id.back_button);
 //        btn.setOnClickListener(new View.OnClickListener() {
 //            @Override
